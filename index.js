74bd3f8128a6170d88698ca86a4981f35edf4b77
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-09-30 13:39:15 +0200

    fix ternary operator from semi-colon to colon, add colons after lines 86 and 87 statements

--- index.js
+++ index.js
@@ -82,9 +82,9 @@ function fetchAndDisplayBoardsAndTasks() {
   displayBoards(boards);
   if (boards.length > 0) {
     const localStorageBoard = JSON.parse(localStorage.getItem("activeBoard"))
-    activeBoard = localStorageBoard ? localStorageBoard ;  boards[0]; 
-    elements.headerBoardName.textContent = activeBoard
-    styleActiveBoard(activeBoard)
+    activeBoard = localStorageBoard ? localStorageBoard :  boards[0]; 
+    elements.headerBoardName.textContent = activeBoard;
+    styleActiveBoard(activeBoard);
     refreshTasksUI();
   }
 }
